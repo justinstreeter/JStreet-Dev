c57b830b5d9bc46bd849ec847ecf67ec8ba6aef9
justinstreeter <justinstreeter1@outlook.com>
2015-12-30 17:38:53 -0500

    new set of avctive projects

--- active_projects.php
+++ active_projects.php
@@ -61,7 +61,7 @@ echo " <div class='col-lg-12'>
       echo " 
     <section id='active_prof'>
      <div class='row'>
-      <div class='col-md-6 col-md-offset-3'>
+      <div class='col-md-12'>
        <div class='row'>
         <div class='thumbnail'>
          <div class='col-md-6'>
