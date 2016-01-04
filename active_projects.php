af2e47434b26ca68a5282e0144767dc7a0b5a954
justinstreeter <justinstreeter1@outlook.com>
2016-01-04 09:57:10 -0500

    change to active projects admin section for individual profiles

--- active_projects.php
+++ active_projects.php
@@ -46,8 +46,8 @@ echo " <div class='col-lg-12'>
 
 
 }else{
-        
-         $sql = "SELECT u.fname, u.lname, p.profile_img, ap.user_id, ap.screenshot_img, ap.update_info, ap.date_update, ap.start_date FROM users as u INNER JOIN profile as p on u.id = p.user_id INNER JOIN active_projects as ap on u.id = ap.user_id ORDER BY ap.date_update";
+        $userid = $_POST['uid'];
+         $sql = "SELECT u.id, u.fname, u.lname, p.profile_img, ap.screenshot_img, ap.date_update, pn.project_names, pn.start_date, ap.update_info FROM users as u INNER JOIN profile as p ON u.id = p.user_id INNER JOIN active_projects as ap INNER JOIN project_name as pn ON ap.project_id = pn.project_id WHERE u.id = '$userid' ORDER BY pn.project_id ";
         
     $profile = $conn->query($sql);
     
@@ -59,12 +59,26 @@ echo " <div class='col-lg-12'>
     foreach ($profile as $pro):
     
       echo " 
-     <section id='active_prof'>
+      <section id='active_prof'>
      <div class='row'>
       <div class='col-md-12'>
-       <div class='row' id='active_section'>
-        <div>
-         <div class='col-md-6' style='margin: 50px 0px 50px 0px'>
+          <div class='row'>
+          <div class='col-md-6 col-md-offset-3' id='active-col' >    
+          <div class='col-md-3'>
+          <div class='profile_small_img' style='background-image: url(uploads/".$pro['profile_img']; echo ")'>
+          </div>
+          </div>
+          <div class='col-md-9'>
+              <h3 id='profile_name'>".$pro['fname']; echo "&nbsp".$pro['lname']."</h3>
+              <p style='text-align:center;'> Date of Project Start: ".$pro['start_date']; echo "</p>
+              </div>
+              </div>
+          </div>
+          
+          
+       <div class='row' >
+       
+         <div class='col-md-6 col-md-offset-3 ' id='active-col' style='margin-top:50px; margin-bottom: 50px; '>
          <h3>Progress</h3>
           <form class='thumb_img' id='profile_form' action='img_upload.php' method='post' enctype='multipart/form-data'>
             <div class='screenshot_img' style='background-image: url(uploads/".$pro['screenshot_img']; echo ")'>
@@ -73,21 +87,13 @@ echo " <div class='col-lg-12'>
             </div>
             <p style='text-align:center; padding-top:10px;'> Date Updated: ".$pro['date_update']; echo "</p>
             </form>
-          </div>
-          <div class='col-md-6' style='height:500px; text-align:center; padding-top:50px;'>
-          <div class='col-md-4'>
-          <div class='profile_small_img' style='background-image: url(uploads/".$pro['profile_img']; echo ")'>
-          </div>
-          </div>
-          <div class='col-md-8'>
-              <h3 id='profile_name'>".$pro['fname']; echo "&nbsp".$pro['lname']."</h3>
-              <p> Date of Project Start: ".$pro['start_date']; echo "</p>
-              </div>
-              <p style='margin: 200px 0px 100px 0px;'>".$pro['update_info']; echo "</p>
-            <p><a href='active_projects.php' class='btn btn-custom2' role='button'>Active Projects</a> <a href='#' class='btn btn-custom' role='button'>Finished Projects</a></p>    
-            </div>
+          <p style='text-align:center;'>".$pro['update_info']; echo "</p>
+            <p style='text-align:center;'><a href='active_edit.php' class='btn btn-custom2' role='button'>Make Update</a></p>    
          </div>
+         
         </div>
+        
+        
        </div>
       </div>
      </section>  
--- profiles.php
+++ profiles.php
@@ -14,13 +14,16 @@ if ( @$_SESSION['groups'] != "admin"){
     
    echo " <div class='col-lg-12'>
         <h1 id='profile_heading'>Profile</h1>
-        </div>";
+        </div>
+        
+        <section id='user_profile'>
+    <div class='row'>
+            ";
     
     foreach ($profile as $pro): 
 
-   echo "<section id='user_profile'>
-    <div class='row'>
-       <div class='col-md-6 col-md-offset-3'>
+   echo "
+       <div class='col-sm-6 col-md-4'>
         <div class='thumbnail'>
             <form class='thumb_img' id='profile_form' action='img_upload.php' method='post' enctype='multipart/form-data'>
             <div class='profile_img' style='background-image: url(uploads/".$pro['profile_img']; echo ")'>
@@ -35,26 +38,27 @@ if ( @$_SESSION['groups'] != "admin"){
             </div>
             </div>
         </div>
-        </div>
-    </section>";
+        ";
                     
  endforeach;
-    
+   echo " </div>
+    </section>";
 }else{
 
-    $sql = "SELECT profile.profile_img, profile.profile_info, profile.id, users.uname, users.email, users.fname, users.lname, users.groups, users.date FROM users INNER JOIN profile ON users.id=profile.user_id ORDER BY users.id";
+    $sql = "SELECT profile.profile_img, profile.profile_info, users.id, users.uname, users.email, users.fname, users.lname, users.groups, users.date FROM users INNER JOIN profile ON users.id=profile.user_id ORDER BY users.id";
     
     $profile = $conn->query($sql);
     
    echo " <div class='col-lg-12'>
         <h1 id='profile_heading'>Profiles</h1>
-        </div>";
+        </div>
+        <section id='user_profile'>
+    <div class='row'>";
     
     foreach ($profile as $pro): 
 
-   echo "<section id='user_profile'>
-    <div class='row'>
-       <div class='col-md-6 col-md-offset-3'>
+   echo "
+       <div class='col-sm-6 col-md-4'>
         <div class='thumbnail'>
             <form class='thumb_img' id='profile_form' action='img_upload.php' method='post' enctype='multipart/form-data'>
             <div class='profile_img' style='background-image: url(uploads/".$pro['profile_img']; echo ")'>
@@ -66,15 +70,18 @@ if ( @$_SESSION['groups'] != "admin"){
             <p> Date Registered: ".$pro['date']; echo "</p>
             <h3 id='profile_name'>".$pro['fname']; echo "&nbsp".$pro['lname']."</h3>
             <p>".$pro['profile_info']; echo "</p>
-             <p><a href='active_projects.php' class='btn btn-custom2' role='button'>Active Projects</a> <a href='#' class='btn btn-custom' role='button'>Finished Projects</a></p>    
+            <form action='active_projects.php' method='post'>
+            <input type='hidden' name='uid' value='".$pro['id']; echo "'>
+             <p><input class='btn btn-custom2' type='submit' value='Active Projects'/> <a href='#' class='btn btn-custom' role='button'>Finished Projects</a></p>    
+             </form>
             </div>
             </div>
         </div>
-        </div>
-    </section>";
+        ";
                     
  endforeach;
-     
+     echo "</div>
+    </section>";
 }
 
 
